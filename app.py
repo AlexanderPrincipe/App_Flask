afb526761f07280aa8b6326157affe91fd3615cc
Alexander Principe <principetolentinoa@hotmail.com>
2019-07-31 12:41:18 -0500

    Avance2

--- app.py
+++ app.py
@@ -22,6 +22,19 @@ def create():
     db.session.commit()
     return redirect(url_for('home'))
 
+@app.route('/done/<id>')
+def done(id):
+    task = Task.query.filter_by(id=int(id)).first()
+    task.done = not(task.done)
+    db.session.commit()
+    return  redirect(url_for('home'))
+
+@app.route('/delete/<id>')
+def delete(id):
+    task = Task.query.filter_by(id=int(id)).delete()
+    db.session.commit()
+    return redirect(url_for('home'))
+
 
 if __name__ == '__main__':
     app.run(debug=True)
